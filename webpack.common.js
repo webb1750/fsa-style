7efcc45680eb2026d829a5e2a359827a9aa3f144
Casey Hill <bignamehere@gmail.com>
2018-06-11 15:35:12 -0500

    updated wp4 example to include HTML Prettify [appends #247]

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLBeautifyPlugin = require('html-beautify-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
@@ -99,6 +100,19 @@ module.exports = {
       options:{
         title: 'FSA Style - Boilerplate'
       }
+    }),
+    new HTMLBeautifyPlugin({
+      config: {
+          html: {
+              end_with_newline: true,
+              indent_size: 2,
+              indent_with_tabs: true,
+              indent_inner_html: true,
+              preserve_newlines: true,
+              unformatted: ['p', 'i', 'b', 'span']
+          }
+      },
+      replace: [ ' type="text/javascript"' ]
     }),
     new CopyWebpackPlugin([
       {
